1ef1ede651986b7ce6435660667bb769aaf716d4
Maxim Zelenkin <nudepatch@gmail.com>
2022-06-29 04:22:29 +0500

    Lesson 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
 
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
-    HelloWorld,
+    EventCard,
   },
-};
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          category: 'Cate',
+          title: 'Some title',
+          description: 'Description',
+          location: '',
+          time: '16pm',
+          date: 'January 1st',
+          petsAllowed: true,
+          organizer: 'Org',
+        },
+        {
+          id: 2,
+          category: 'Cate',
+          title: 'Some title',
+          description: 'Description',
+          location: '',
+          time: '16pm',
+          date: 'January 1st',
+          petsAllowed: true,
+          organizer: 'Org',
+        },
+      ],
+    }
+  },
+}
 </script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
