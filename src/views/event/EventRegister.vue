691d8a09660d745d69b7a4b55bd69869b32d9338
Maxim Zelenkin <nudepatch@gmail.com>
2022-06-29 22:49:39 +0500

    Touring Vue Router Lesson 6

--- src/views/event/EventRegister.vue
+++ src/views/event/EventRegister.vue
@@ -1,9 +1,22 @@
 <template>
   <p>Register for Event here</p>
+  <button @click="register">Register me</button>
 </template>
 
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      // this.$router.replace will not save in history
+      // this.$router.go(1) - Browser forward button
+      // this.$router.go(-1) - Browser back button
+      this.$router.push({
+        name: 'EventDetails',
+        // Pupulated by default because it's nested view
+        // params: { id: this.event.id },
+      })
+    },
+  },
 }
 </script>
