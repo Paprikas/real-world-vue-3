eb7f76f2ea121bfa41819994c7bf6d851410afe3
Maxim Zelenkin <nudepatch@gmail.com>
2022-06-29 05:36:44 +0500

    Lesson 6

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
 import AboutView from '../views/AboutView.vue'
 
 const routes = [
@@ -9,8 +10,14 @@ const routes = [
     component: EventList,
   },
   {
+    path: '/events/:id',
+    name: 'EventDetails',
+    component: EventDetails,
+    props: true,
+  },
+  {
     path: '/about',
-    name: 'about',
+    name: 'AboutView',
     component: AboutView,
   },
 ]
