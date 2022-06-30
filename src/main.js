2accce02e0a0b6c50cba744e0a1a691a588ef9a8
Maxim Zelenkin <nudepatch@gmail.com>
2022-07-01 02:41:55 +0500

    From Vuex to Pinia Lesson 4

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import Store from './store'
 import 'nprogress/nprogress.css'
 
-createApp(App).use(router).use(Store).mount('#app')
+createApp(App).use(router).use(Store).use(createPinia()).mount('#app')
