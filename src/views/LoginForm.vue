43d13a81bd6acd5eba476b62c4983d151511adf6
Maxim Zelenkin <nudepatch@gmail.com>
2022-07-02 06:58:05 +0500

    Validating Vue 3 Forms Lesson 3

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -2,14 +2,19 @@
   <form @submit.prevent="onSubmit">
     <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput label="Password" type="password" />
+    <BaseInput
+      label="Password"
+      type="password"
+      v-model="password"
+      :error="passwordError"
+    />
 
     <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
 <script>
-import { useField } from 'vee-validate'
+import { useField, useForm } from 'vee-validate'
 
 export default {
   setup() {
@@ -17,9 +22,8 @@ export default {
       alert('Submitted')
     }
 
-    const { value: email, errorMessage: emailError } = useField(
-      'email',
-      function (value) {
+    const validations = {
+      email: (value) => {
         if (!value) return 'This field is required'
 
         const regex =
@@ -30,12 +34,31 @@ export default {
         }
 
         return true
-      }
-    )
+      },
+      password: (value) => {
+        const requiredMessage = 'This field is required'
+
+        if (value === undefined || value === null) return requiredMessage
+        if (!String(value).length) return requiredMessage
+
+        return true
+      },
+    }
+
+    useForm({
+      validationSchema: validations,
+    })
+
+    const { value: email, errorMessage: emailError } = useField('email')
+    const { value: password, errorMessage: passwordError } =
+      useField('password')
+
     return {
       onSubmit,
-      email: email,
-      emailError: emailError,
+      email,
+      emailError,
+      password,
+      passwordError,
     }
   },
 }
